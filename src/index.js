06ca9ef74f935fa0eb475af808d8d6709cf761b0
Mdhuna r <rmidhuna4@gmail.com>
2025-02-18 11:42:56 +0530

    cors issue 2

--- src/index.js
+++ src/index.js
@@ -8,31 +8,20 @@ import config from "./config/config.js";
 
 
 const app = express();
-const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = ["https://task-frontend-bice-alpha.vercel.app/"];
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-    credentials: true
-};
 
-// Connect to Database
+
 dbConnect();
-
-// Middlewares
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'))
 app.use(express.static("public/"));
 
+
+app.use(cors({
+    origin: config.API_URL,
+    credentials: true
+}))
 app.use("/api", userRoutes);
 
 app.listen(config.PORT, () => {
